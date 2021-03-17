9420f3336900359edfba736a1d6d67b54b24398e
hapi190601 <gc1206ro@gmail.com>
2021-03-17 06:48:17 +0000

    first:product_index

--- app/controllers/public/products_controller.rb
+++ app/controllers/public/products_controller.rb
@@ -6,8 +6,12 @@ class Public::ProductsController < ApplicationController
   end
 
   def index
+    @products = Product.page(params[:page]).per(8).order(updated_at: :desc)
+    @tax = 1.1
   end
 
   def show
+    @product = Product.find(params[:id])
+
   end
 end
