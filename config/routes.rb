02dab99ee6f7c47c3c215acb22ce4d3330911cb0
EC2 Default User <ec2-user@ip-172-31-48-192.ec2.internal>
2021-03-17 06:17:36 +0000

    [add]about.html

--- config/routes.rb
+++ config/routes.rb
@@ -3,15 +3,15 @@ Rails.application.routes.draw do
 
 
 
-    
+
 
   scope module: :public do
     resources :members, only: [:show,:update,:edit]
     get 'members/withdraw' => "members#withdraw"
     patch 'members/out' => "members#out"
-    resources :products, only:[:index,:show]
-    root to: "products#top" 
+    root to: "products#top"
     get 'products/about' => "products#about"
+    resources :products, only:[:index,:show]
     get 'searches/search' => "searches#search"
     resources :orders, only:[:index,:show,:create]
     get 'orders/complete' => "orders#complete"
