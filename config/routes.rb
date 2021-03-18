e9a69e2154dfeb1843fb6dcd29b5fe7286f61e4e
Takashi-1027 <taka19921027@gmail.com>
2021-03-18 02:54:02 +0000

    [update]

--- config/routes.rb
+++ config/routes.rb
@@ -9,9 +9,9 @@ Rails.application.routes.draw do
     resources :members, only: [:show,:update,:edit]
     get 'members/withdraw' => "members#withdraw"
     patch 'members/out' => "members#out"
-    resources :products, only:[:index,:show]
     root to: "products#top" 
     get 'products/about' => "products#about"
+    resources :products, only:[:index,:show]
     get 'searches/search' => "searches#search"
     resources :orders, only:[:index,:show,:create]
     get 'orders/complete' => "orders#complete"
