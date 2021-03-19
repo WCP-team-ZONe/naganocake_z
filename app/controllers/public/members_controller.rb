ad0cab307cf6b66dbae03de25fe40d5b73b6eba0
EC2 Default User <ec2-user@ip-172-31-48-192.ec2.internal>
2021-03-19 13:12:08 +0000

    member-edit

--- app/controllers/public/members_controller.rb
+++ app/controllers/public/members_controller.rb
@@ -10,8 +10,21 @@ class Public::MembersController < ApplicationController
   end
 
   def update
+    @member = current_member
+      if @member.update(member_params)
+        redirect_to member_path
+      else
+        render :edit
+      end
   end
 
   def edit
+    @member = current_member
   end
+
+  private
+  def member_params
+    params.require(:member).permit(:family_name, :first_name, :family_kana, :first_kana, :postal_code, :address, :phone_number, :email,)
+  end
+
 end
