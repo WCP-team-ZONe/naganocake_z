25fe2f244af5d2eef534382079919e232240da61
YukiIshkikawa1 <ishikawayuki0304@gmail.com>
2021-03-19 01:46:32 +0000

    [update]エラーの解消,コードの追記

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,18 @@ class ApplicationController < ActionController::Base
 
   protected
 
+  def after_sign_in_path_for(resource)
+    if member_signed_in?
+      root_path
+    else
+      private_orders_path
+    end
+  end
+
+  def after_sign_out_path_for(resource)
+    root_path
+  end
+
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:family_name, :first_name, :family_kana, :first_kana, :postal_code, :address, :phone_number, :is_deleted])
     devise_parameter_sanitizer.permit(:sign_in, keys: [:email])
