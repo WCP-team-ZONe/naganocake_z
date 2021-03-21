fa6bc1557c069481e6d07876b2601acc930c7476
YukiIshkikawa1 <ishikawayuki0304@gmail.com>
2021-03-21 08:32:22 +0000

    [update]

--- app/models/product.rb
+++ app/models/product.rb
@@ -6,7 +6,7 @@ class Product < ApplicationRecord
   validates :name, uniqueness: true
   validates :description, presence: true
   validates :ex_price, presence: true
-  validates :is_active, presence: true, inclusion: { in: [true, false] }
+  validates :is_active, inclusion: { in: [true, false] }
 
   attachment :image
 end
