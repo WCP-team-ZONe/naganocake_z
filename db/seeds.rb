eea606b3ce9e00509686256136313482aedecb86
大谷 凌介 <65483033+hapi190601@users.noreply.github.com>
2021-03-22 14:56:44 +0900

    Update seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,6 +6,7 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
+
 #ジャンル名
 2.times do |n|
     Genre.create!(
