c4b34e97135b41e9bb06babc1163aff5a4de5fb4
hapi190601 <gc1206ro@gmail.com>
2021-03-22 06:12:21 +0000

    Finish-order-index

--- app/controllers/public/orders_controller.rb
+++ app/controllers/public/orders_controller.rb
@@ -19,6 +19,9 @@ class Public::OrdersController < ApplicationController
       @order.address = @my_address
       @order.name = @my_name
     elsif params[:order][:option] == "1"
+      # if params[:order][:address_id].none?
+      #   render "check"
+      # end
       shipping_address = Address.find(params[:order][:address_id])
       @order.postal_code = shipping_address.postal_code
       @order.address = shipping_address.address
@@ -31,27 +34,32 @@ class Public::OrdersController < ApplicationController
 
   def create
     # ordersテーブルに保存するアクション
-    @order = Order.new(order_params)
+    @order = Order.new(new_order_params)
     @order.member_id = current_member.id
     @order.postage = @postage
     @order.save
 
+    # 配送先を保存するアクション
+    @address = Address.new(address_params)
+    @address.member_id = current_member.id
+    @address.save
+
     # ordered_productsテーブルに保存するアクション
     @cart_products = CartProduct.where(member_id: current_member.id)
-      # @cart_products.each do |cart_product|
-      #   # 注文商品テーブルに保存する空箱を作る
-      #   @ordered_product = @order.ordered_products.new
-      #   # カート商品が持つ注文idを代入
-      #   @ordered_product.order_id = cart_product.id
-      #   # カート商品が持つ商品idを代入
-      #   @ordered_product.product_id = cart_product.product_id
-      #   # カート商品が持つ商品個数を代入
-      #   @ordered_product.quantity = cart_product.quantity
-      #   # カート商品が持つ税抜価格を代入
-      #   @ordered_product.purchased_price = cart_product.ex_price
-      #   # 注文商品テーブルに保存
-      #   @ordered_product.save
-      # end
+      @cart_products.each do |cart_product|
+        # 注文商品テーブルに保存する空箱を作る
+        @ordered_product = @order.ordered_products.new
+        # カート商品が持つ注文idを代入
+        @ordered_product.order_id = @order.id
+        # カート商品が持つ商品idを代入
+        @ordered_product.product_id = cart_product.product_id
+        # カート商品が持つ商品個数を代入
+        @ordered_product.quantity = cart_product.quantity
+        # カート商品が持つ税抜価格を代入
+        @ordered_product.purchased_price = cart_product.product.ex_price
+        # 注文商品テーブルに保存
+        @ordered_product.save
+      end
     # カート内データ全消去
     @cart_products.destroy_all
     redirect_to orders_complete_path
@@ -60,13 +68,13 @@ class Public::OrdersController < ApplicationController
   def complete
   end
 
-
   def index
     @orders = Order.where(member_id: current_member.id)
   end
 
   def show
     @order = Order.find(params[:id])
+    @ordered_products = OrderedProduct.where(order_id: @order.id)
   end
 
   def my_info
@@ -78,6 +86,14 @@ class Public::OrdersController < ApplicationController
 # ストロングパラメータ
   private
   def order_params
-    params.permit(:payment, :postal_code, :address, :name, :total_price)
+    params.require(:order).permit(:payment, :postal_code, :address, :name, :total_price)
+  end
+
+  def new_order_params
+    params.require(:order).permit(:payment, :postal_code, :address, :name, :total_price)
+  end
+
+  def address_params
+    params.permit(:postal_code, :address, :name)
   end
 end
